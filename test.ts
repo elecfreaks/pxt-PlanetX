3cdcd90b2b8270605d69c6c7e15bb0f575510e0b
Lionkk <Lion_yan@outlook.com>
2020-04-18 17:13:39 +0800

    Updates.

--- test.ts
+++ test.ts
@@ -1,7 +1,10 @@
-// tests go here; this will not be compiled when this package is used as a library
-
-
-
-
-
-
+OLED.init(128, 64)
+basic.forever(function () {
+    if (Stars.buttonAB(Stars.DigitalRJPin.J3, Stars.ButtonStateList.B)) {
+        OLED.writeNum(Stars.LightSensor(Stars.AnalogRJPin.J1))
+        OLED.writeNum(Stars.NoiseSensor(Stars.AnalogRJPin.J2))
+        basic.pause(1000)
+    } else if (Stars.buttonAB(Stars.DigitalRJPin.J3, Stars.ButtonStateList.B)) {
+        Stars.Relay(Stars.DigitalRJPin.J4, Stars.RelayStateList.On)
+    }
+})

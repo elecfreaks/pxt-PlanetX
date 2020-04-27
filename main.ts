ec527fade746a50578e5115d2342c86b37a5e411
Lionkk <Lion_yan@outlook.com>
2020-04-27 16:03:45 +0800

    Updates.

--- main.ts
+++ main.ts
@@ -4,7 +4,6 @@
 //% color=#00B1ED  icon="\uf005" block="PlanetX" blockId="Planet_X"
 //% groups='["LED", "8*16 Matrix", "Digital", "Analog", "IIC Port", "7-Seg 4-Dig LED Nixietube"]'
 namespace PlanetX {
-
     ///////////////////////////// BME280 
     let BME280_I2C_ADDR = 0x76
     let dig_T1 = getUInt16LE(0x88)

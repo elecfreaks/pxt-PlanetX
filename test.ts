11b52985b04c10425df113fc5192624f012ee931
Lionkk <Lion_yan@outlook.com>
2020-04-30 14:50:23 +0800

    Updates.

--- test.ts
+++ test.ts
@@ -1,10 +1,1 @@
-OLED.init(128, 64)
-basic.forever(function () {
-    if (PlanetX.buttonAB(PlanetX.DigitalRJPin.J3, PlanetX.ButtonStateList.B)) {
-        OLED.writeNum(PlanetX.LightSensor(PlanetX.AnalogRJPin.J1))
-        OLED.writeNum(PlanetX.NoiseSensor(PlanetX.AnalogRJPin.J2))
-        basic.pause(1000)
-    } else if (PlanetX.buttonAB(PlanetX.DigitalRJPin.J3, PlanetX.ButtonStateList.B)) {
-        PlanetX.Relay(PlanetX.DigitalRJPin.J4, PlanetX.RelayStateList.On)
-    }
-})
+ 

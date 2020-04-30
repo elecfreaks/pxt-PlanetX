0754c70898e932f3f250dae8c74adf0980e22f0d
Lionkk <Lion_yan@outlook.com>
2020-04-30 15:38:55 +0800

    Updates.

--- main.ts
+++ main.ts
@@ -1264,214 +1264,6 @@ namespace PlanetX {
      */
     //% blockId=grove_tm1637_create block="connect 4-Digit Display |pin %pin|"
     //% subcategory=Display group="7-Seg 4-Dig LED Nixietube" blockSetVariable=display
-    export function createDisplay(Rjpin: DigitalRJPin): TM1637 {
-        let display = new TM1637()
-        switch (Rjpin) {
-            case DigitalRJPin.J1:
-                display.clkPin = DigitalPin.P1
-                display.dataPin = DigitalPin.P8
-                break;
-            case DigitalRJPin.J2:
-                display.clkPin = DigitalPin.P2
-                display.dataPin = DigitalPin.P12
-                break;
-            case DigitalRJPin.J3:
-                display.clkPin = DigitalPin.P13
-                display.dataPin = DigitalPin.P14
-                break;
-            case DigitalRJPin.J4:
-                display.clkPin = DigitalPin.P15
-                display.dataPin = DigitalPin.P16
-                break;
-        }
-        display.buf = pins.createBuffer(4)
-        display.brightnessLevel = 7
-        display.pointFlag = false
-        display.clear()
-
-        return display
-    }
-    export class TM1637 {
-        clkPin: DigitalPin
-        dataPin: DigitalPin
-        brightnessLevel: number
-        pointFlag: boolean
-        buf: Buffer
-
-        private writeByte(wrData: number) {
-            for (let i = 0; i < 8; i++) {
-                pins.digitalWritePin(this.clkPin, 0)
-                if (wrData & 0x01) pins.digitalWritePin(this.dataPin, 1)
-                else pins.digitalWritePin(this.dataPin, 0)
-                wrData >>= 1
-                pins.digitalWritePin(this.clkPin, 1)
-            }
-
-            pins.digitalWritePin(this.clkPin, 0) // Wait for ACK
-            pins.digitalWritePin(this.dataPin, 1)
-            pins.digitalWritePin(this.clkPin, 1)
-        }
-
-        private start() {
-            pins.digitalWritePin(this.clkPin, 1)
-            pins.digitalWritePin(this.dataPin, 1)
-            pins.digitalWritePin(this.dataPin, 0)
-            pins.digitalWritePin(this.clkPin, 0)
-        }
-
-        private stop() {
-            pins.digitalWritePin(this.clkPin, 0)
-            pins.digitalWritePin(this.dataPin, 0)
-            pins.digitalWritePin(this.clkPin, 1)
-            pins.digitalWritePin(this.dataPin, 1)
-        }
-
-        private coding(dispData: number): number {
-            let pointData = 0
-
-            if (dispData == 0x7f) dispData = 0x00
-            else if (dispData == 0x3f) dispData = 0x3f
-            else dispData = TubeTab[dispData] + pointData
-
-            return dispData
-        }
-
-        /**
-         * Show a 4 digits number on display
-         * @param dispData value of number
-         */
-        //% blockId=grove_tm1637_display_number block="%display|show number|%dispData"
-        //% subcategory=Display group="7-Seg 4-Dig LED Nixietube"
-        show(dispData: number, fillWithZeros = false) {
-            let def = 0x7f
-            if (fillWithZeros)
-                def = 0x3f
-            if (dispData < 10) {
-                this.bit(dispData, 3)
-                this.bit(def, 2)
-                this.bit(def, 1)
-                this.bit(def, 0)
-
-                this.buf[3] = dispData
-                this.buf[2] = def
-                this.buf[1] = def
-                this.buf[0] = def
-            }
-            else if (dispData < 100) {
-                this.bit(dispData % 10, 3)
-                this.bit((dispData / 10) % 10, 2)
-                this.bit(def, 1)
-                this.bit(def, 0)
-
-                this.buf[3] = dispData % 10
-                this.buf[2] = (dispData / 10) % 10
-                this.buf[1] = def
-                this.buf[0] = def
-            }
-            else if (dispData < 1000) {
-                this.bit(dispData % 10, 3)
-                this.bit((dispData / 10) % 10, 2)
-                this.bit((dispData / 100) % 10, 1)
-                this.bit(def, 0)
-
-                this.buf[3] = dispData % 10
-                this.buf[2] = (dispData / 10) % 10
-                this.buf[1] = (dispData / 100) % 10
-                this.buf[0] = def
-            }
-            else {
-                this.bit(dispData % 10, 3)
-                this.bit((dispData / 10) % 10, 2)
-                this.bit((dispData / 100) % 10, 1)
-                this.bit((dispData / 1000) % 10, 0)
-
-                this.buf[3] = dispData % 10
-                this.buf[2] = (dispData / 10) % 10
-                this.buf[1] = (dispData / 100) % 10
-                this.buf[0] = (dispData / 1000) % 10
-            }
-        }
-
-        /**
-         * Set the brightness level of display at from 0 to 7
-         * @param level value of brightness level
-         */
-        //% blockId=grove_tm1637_set_display_level block="%display|brightness level to|%level"
-        //% level.min=0 level.max=7
-        //% subcategory=Display group="7-Seg 4-Dig LED Nixietube"
-        set(level: number) {
-            this.brightnessLevel = level
-
-            this.bit(this.buf[0], 0x00)
-            this.bit(this.buf[1], 0x01)
-            this.bit(this.buf[2], 0x02)
-            this.bit(this.buf[3], 0x03)
-        }
-
-        /**
-         * Show a single number from 0 to 9 at a specified digit of Grove - 4-Digit Display
-         * @param dispData value of number
-         * @param bitAddr value of bit number
-         */
-        //% blockId=grove_tm1637_display_bit block="%display|show single number|%dispData|at digit|%bitAddr"
-        //% dispData.min=0 dispData.max=9
-        //% bitAddr.min=0 bitAddr.max=3
-        //% subcategory=Display group="7-Seg 4-Dig LED Nixietube"
-        bit(dispData: number, bitAddr: number) {
-            if ((dispData == 0x7f) || (dispData == 0x3f) || ((dispData <= 9) && (bitAddr <= 3))) {
-                let segData = 0
-
-                if (bitAddr == 1 && this.pointFlag)
-                    segData = this.coding(dispData) + 0x80
-                else
-                    segData = this.coding(dispData)
-                this.start()
-                this.writeByte(0x44)
-                this.stop()
-                this.start()
-                this.writeByte(bitAddr | 0xc0)
-                this.writeByte(segData)
-                this.stop()
-                this.start()
-                this.writeByte(0x88 + this.brightnessLevel)
-                this.stop()
-
-                this.buf[bitAddr] = dispData
-            }
-        }
-
-        /**
-         * Turn on or off the colon point on Grove - 4-Digit Display
-         * @param pointEn value of point switch
-         */
-        //% blockId=grove_tm1637_display_point block="%display|turn|%point|colon point"
-        //% subcategory=Display group="7-Seg 4-Dig LED Nixietube"
-        point(b: boolean) {
-
-            this.pointFlag = b
-            this.bit(this.buf[1], 0x01)
-        }
-
-        /**
-         * Clear the display
-         */
-        //% blockId=grove_tm1637_display_clear block="%display|clear"
-        //% subcategory=Display group="7-Seg 4-Dig LED Nixietube"
-        clear() {
-            this.bit(0x7f, 0x00)
-            this.bit(0x7f, 0x01)
-            this.bit(0x7f, 0x02)
-            this.bit(0x7f, 0x03)
-        }
-    }
-
-    /**
-     * Create a new driver Grove - 4-Digit Display
-     * @param clkPin value of clk pin number
-     * @param dataPin value of data pin number
-     */
-    //% blockId=grove_tm1637_create block="connect 4-Digit Display |pin %pin|"
-    //% subcategory=Display group="7-Seg 4-Dig LED Nixietube" blockSetVariable=display
     export function create(Rjpin: DigitalRJPin, intensity: number=7, count: number=4): TM1637LEDs {
         let display = new TM1637LEDs();
         switch (Rjpin) {
@@ -1564,20 +1356,7 @@ namespace PlanetX {
             pins.digitalWritePin(this.clk, 1);
             pins.digitalWritePin(this.clk, 0);
         }
-
-        /**
-         * set TM1637 intensity, range is [0-8], 0 is off.
-         * @param val the brightness of the TM1637, eg: 7
-         */
-        //% blockId="TM1637_set_intensity" block="%tm|set intensity %val"
-        //% weight=50 blockGap=8
-        //% parts="TM1637"
-        intensity(val: number = 7) {
-            if (val < 1) {
-                this.off();
-                return;
-            }
-            if (val > 8) val = 8;
+        _intensity(val: number = 7) {
             this._ON = 8;
             this.brightness = val - 1;
             this._write_data_cmd();
@@ -1629,25 +1408,6 @@ namespace PlanetX {
         }
 
         /**
-          * show a hex number. 
-          * @param num is a hex number, eg: 0
-          */
-        //% blockId="TM1637_showhex" block="%tm|show hex number %num"
-        //% weight=90 blockGap=8
-        //% parts="TM1637"
-        showHex(num: number) {
-            if (num < 0) {
-                this._dat(0, 0x40) // '-'
-                num = -num
-            }
-            else
-                this.showbit((num >> 12) % 16)
-            this.showbit(num % 16, 3)
-            this.showbit((num >> 4) % 16, 2)
-            this.showbit((num >> 8) % 16, 1)
-        }
-
-        /**
          * show or hide dot point. 
          * @param bit is the position, eg: 1
          * @param show is show/hide dp, eg: true
@@ -1674,29 +1434,7 @@ namespace PlanetX {
             }
         }
 
-        /**
-         * turn on LED. 
-         */
-        //% blockId="TM1637_on" block="turn on %tm"
-        //% weight=86 blockGap=8
-        //% parts="TM1637"
-        on() {
-            this._ON = 8;
-            this._write_data_cmd();
-            this._write_dsp_ctrl();
-        }
 
-        /**
-         * turn off LED. 
-         */
-        //% blockId="TM1637_off" block="turn off %tm"
-        //% weight=85 blockGap=8
-        //% parts="TM1637"
-        off() {
-            this._ON = 0;
-            this._write_data_cmd();
-            this._write_dsp_ctrl();
-        }
     }
 
 

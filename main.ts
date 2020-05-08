3b064b94f48d81e80f82bd890a02053517cf39c7
Lionkk <Lion_yan@outlook.com>
2020-05-08 10:50:25 +0800

    Updates.

--- main.ts
+++ main.ts
@@ -1387,10 +1387,6 @@ namespace PlanetX {
     //% block="clear display"
     //% subcategory=Display group="OLED" color=#EA5532
     export function oledclear() {
-        if (firstoledinit) {
-            oledinit()
-            firstoledinit = false
-        }
         //oledcmd(DISPLAY_OFF);   //display off
         for (let j = 0; j < 8; j++) {
             setText(j, 0);

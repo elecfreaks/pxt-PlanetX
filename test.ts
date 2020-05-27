a1ba7befec8a345b3cd56f991c64fa832d364bfa
Lionkk <Lion_yan@outlook.com>
2020-05-27 15:12:33 +0800

    Updates.

--- test.ts
+++ test.ts
@@ -1,6 +1,6 @@
  basic.forever(function () {
-    PlanetX.showUserText(1, "Temp:" + PlanetX.bme280Sensor(PlanetX.BME280_state.BME280_temperature_C))
-    PlanetX.showUserText(2, "Distance:" + PlanetX.ultrasoundSensor(PlanetX.DigitalRJPin.J1, PlanetX.Distance_Unit_List.Distance_Unit_cm))
+    PlanetX_Display.showUserText(1, "Temp:" + PlanetX.bme280Sensor(PlanetX.BME280_state.BME280_temperature_C))
+    PlanetX_Display.showUserText(2, "Distance:" + PlanetX.ultrasoundSensor(PlanetX.DigitalRJPin.J1, PlanetX.Distance_Unit_List.Distance_Unit_cm))
     if (PlanetX.buttonAB(PlanetX.DigitalRJPin.J1, PlanetX.ButtonStateList.A)) {
         PlanetX.motorFan(PlanetX.AnalogRJPin.J1, true, 80)
     }

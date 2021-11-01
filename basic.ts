5b66cc80c1a3c635c259c6fc348fd6801e3fbd59
ELECFREAKS.Admin <support@elecfreaks.com>
2021-11-01 18:57:12 +0800

    更新 pxt.json, basic.ts

--- basic.ts
+++ basic.ts
@@ -1116,6 +1116,7 @@ namespace PlanetX_Basic {
     {
         pins.i2cWriteNumber(0x1a, 4, NumberFormat.Int8LE)
         TrackBit_state_value = pins.i2cReadNumber(0x1a, NumberFormat.UInt8LE, false)
+        basic.pause(5);
     }
 
     //% blockId="readdht11" block="DHT11 sensor %Rjpin %dht11state value"

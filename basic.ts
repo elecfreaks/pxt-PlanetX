38bdfd408891d29c4223844754deab4deae5e3a6
ELECFREAKS.Admin <support@elecfreaks.com>
2021-12-20 19:06:23 +0800

    更改超声波异常处理程序

--- basic.ts
+++ basic.ts
@@ -895,6 +895,8 @@ namespace PlanetX_Basic {
         }
     }
 
+    let distance_last = 0
+
     //% blockId=sonarbit block="Ultrasonic sensor %Rjpin distance %distance_unit"
     //% Rjpin.fieldEditor="gridpicker"
     //% Rjpin.fieldOptions.columns=2
@@ -939,22 +941,13 @@ namespace PlanetX_Basic {
 
         if (distance == 0)
         {
-            basic.pause(50);
-
-            pins.digitalWritePin(pinT, 0)
-            control.waitMicros(2)
-            pins.digitalWritePin(pinT, 1)
-            control.waitMicros(10)
-            pins.digitalWritePin(pinT, 0)
-
-            d = pins.pulseIn(pinE, PulseValue.High, 25000)
-            let distance = d * 9 / 6 / 58
+            distance = distance_last
+            distance_last = 0
         }
-
-        if(distance > 430){
-            distance = 0
+        else
+        {
+            distance_last = distance
         }
-
 
         switch (distance_unit) {
             case Distance_Unit_List.Distance_Unit_cm:

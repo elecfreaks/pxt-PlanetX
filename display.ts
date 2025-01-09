07325625e03cc8af58eb1e63e121344af0b86171
qc <414188617@qq.com>
2025-01-09 15:19:25 +0800

    no msg

--- display.ts
+++ display.ts
@@ -652,7 +652,7 @@ namespace PlanetX_Display {
         }
     }
     
-    //% shim=displaySendBufferAsm
+    //% shim=sendBufferAsm
     declare function sendBuffer(buf: Buffer, pin: DigitalPin): void;
     
     export class Strip {
